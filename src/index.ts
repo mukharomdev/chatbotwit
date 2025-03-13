90713cc741e20708dd69c59e3b54716bfd401c19
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2025-03-13 16:26:41 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import type {ContextApp} from 'chatbotwit'
 
 async function  App(context:ContextApp):Promise<void>{
-  console.log(context.event);
+  //console.log(context.event);
+try{
   await context.sendText('Hello, World!');
+} catch(error){
+alert("error",error);
+}
 };
 
 export default App; // Pastikan app di-export
