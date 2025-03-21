f7c07fbfdc511476fd7945b17889d429c9c2788a
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2025-03-21 02:17:04 +0000

    modified:   src/nlp/converse.ts

--- src/nlp/converse.ts
+++ src/nlp/converse.ts
@@ -1,42 +1,32 @@
 import type {ContextApp} from "chatbotwit"
-// import Message from "./message"
 import ClientWit from "./witconfig"
 
 
-function handleContextMap(ctx:any):WitContext { 
-   
-    let Obj = {[ ctx ]:ctx}
-  
-    return {state:[JSON.stringify(Obj)]}
+function handleContextMap(ctx:any){
+    let regex = "/,/g"
+    let context = {context:[...ctx].join("").replace(regex,'')}
+    return JSON.stringify(context)
 }
-import { WitContext } from "node-wit"
 
 export default async function Converse(context:ContextApp):Promise<any>{
     
     const messageText = context.event.text
     let contextText = {}
-    // Fungsi untuk memproses pesan
+    
+  // Fungsi untuk memproses pesan
     async function process_message(text:string, ctx={}){
-        const WitResponse = ClientWit.message(messageText,ctx)
+        const WitResponse = ClientWit.message(text,ctx)
         const Response = await WitResponse
         //console.log("Wit.ai Response:",Response)
-        
-        // Update context berdasarkan respons
+    
+    // Update context berdasarkan respons
         let new_context = handleContextMap(Response.intents[0].name)
-        return new_context
-    
-    }
         
-    
+        return new_context
+
+    }
+         
     contextText = await process_message(messageText,contextText)
-	//console.log(contextText)
-     
-    // try {    
-    //     await context.sendText(JSON.stringify(contextText))        
-    // } catch(err){
-    //     console.log(err);        
-    // }
-     
     return Promise.resolve(contextText)
     
 }
